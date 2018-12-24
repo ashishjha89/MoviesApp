402a135bcaea6436a61ba881b9530f4f6d4d94d0
Ashish Jha <ashishjha@ebay.com>
2018-12-24 09:01:41 +0100

    Rename ImageManager method params

--- app/src/main/java/moviesnow/com/moviesnow/api/ImageLoader.kt
+++ app/src/main/java/moviesnow/com/moviesnow/api/ImageLoader.kt
@@ -10,15 +10,15 @@ private const val baseImageUrl = "https://image.tmdb.org/t/p/"
 fun loadPosterImage(imagePath: String, imageView: ImageView) {
     val width = "w342"
     val imageUrl = "$baseImageUrl$width$imagePath"
-    loadImage(imageUrl, imageView, R.mipmap.ic_launcher)
+    loadImage(imageUrl = imageUrl, targetImageView = imageView, errorImageId = R.mipmap.ic_launcher)
 }
 
 fun loadBackdropImage(imagePath: String, imageView: ImageView, isExtraWide: Boolean) {
     val width = if (isExtraWide) "w1280" else "w780"
     val imageUrl = "$baseImageUrl$width$imagePath"
-    loadImage(imageUrl, imageView, R.drawable.ic_launcher_background)
+    loadImage(imageUrl = imageUrl, targetImageView = imageView, errorImageId = R.drawable.ic_launcher_background)
 }
 
-private fun loadImage(imageUrl: String, imageView: ImageView, @DrawableRes errorImageId: Int) {
-    Picasso.get().load(imageUrl).error(errorImageId).into(imageView)
+private fun loadImage(imageUrl: String, targetImageView: ImageView, @DrawableRes errorImageId: Int) {
+    Picasso.get().load(imageUrl).error(errorImageId).into(targetImageView)
 }
